43a616273e7e0a2079a39d289cceb23f3293a58d
Unknown <marco.neumann@be-binary.de>
2017-07-12 15:30:30 +0200

    Js Libs added and Script configured
    1. All necessary JS libs were added
    2. The script send_dashboard was configured so it takes aparmeters from splunk and then calls a js script to open a Dashboard and take a screenshot.

---
+++ bin/screenshot.js
@@ -0,0 +1,38 @@
+// modify the login credential here
+var username = 'admin';
+var password = 'admin';
+
+var casper = require('casper').create({ verbose: true, logLevel: 'debug', viewportSize: { width: 1920, height: 600 }, waitTimeout: 10000 });
+
+function removeShadowPath () {
+    var nodes = document.querySelectorAll('*[stroke-opacity]');
+    for (var i = 0; i < nodes.length; i++) {
+        var elem = nodes[i];
+        var strokeOpacity = elem.getAttribute('stroke-opacity');
+        elem.removeAttribute('stroke-opacity');
+        elem.setAttribute('opacity', strokeOpacity);
+    }
+}
+
+if (casper.cli.has(0)) {
+    casper.start(casper.cli.get(0), function () {
+        this.page.paperSize = { format: 'A2', orientation: 'portrait', margin: '1cm' }
+        casper.waitForSelector('form.loginForm', function () {
+            this.fill('form', { username: username, password: password }, true);
+        });
+    });
+
+    var duration = 30000;
+    if (casper.cli.has(1))
+        duration = (+casper.cli.get(1)) * 1000;
+
+    casper.then(function () {
+        this.wait(duration, function () {
+            this.page.evaluate(removeShadowPath);
+            this.capture(casper.cli.has(2) ? casper.cli.get(2) + '.pdf' : 'report.pdf', undefined, { type: 'pdf' });
+            this.capture(casper.cli.has(2) ? casper.cli.get(2) + '.png' : 'report.png');
+        });
+    });
+}
+
+casper.run();
--- bin/send_dashboard.py
+++ bin/send_dashboard.py
@@ -1,13 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import os
 import sys
 import json
 import urllib2
+import urlparse
 import csv
 import gzip
+import subprocess
 from collections import OrderedDict
 
+casper_folder = "casperjs-1.1.4-1"
+phantom_folder = "phantomjs-2.1.1-linux-x86_64"
+# get current directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print >> sys.stderr, "DEBUG Casper: %s, Phantom: %s, Directory: %s" % (casper_folder, phantom_folder, dir_path)
 
-def send_dashboard(session_key, dashboard, app):
-    return True   
+
+def expand_system_path_variable ():
+    '''
+    Expand the System Path variable with phantom js path, so it can be found by casperjs
+    '''
+    os.environ['PATH'] = os.environ['PATH'] + ":" + os.path.join(dir_path, phantom_folder, "bin")
+    print >> sys.stderr, "DEBUG Environment PATH with PhantomJS: %s" % os.environ['PATH']
+
+def get_server_uri (results_link):
+    '''
+    Get Name of server out of the result field of the given payload
+    '''
+    server_uri = urlparse.urlsplit(results_link).scheme + "://" + urlparse.urlsplit(results_link).netloc
+    print >> sys.stderr, "DEBUG Splunk Server URI: %s" % server_uri
+    return server_uri
+
+def build_dashboard_url (server_uri, session_key, dashboard, app) :
+    '''
+    Build the URL to the dashboard
+    '''
+    dashboard_url = server_uri + "/app/" + app +"/" + dashboard
+    print >> sys.stderr, "DEBUG dashboard url: %s" % server_uri
+    return dashboard_url
+
+def create_screenshot_dashboard(dashboard_url):
+    expand_system_path_variable()
+    print >> sys.stderr, "DEBUG Call casperjs for screenshot"   
+    # run casperjs
+    subprocess.call([os.path.join(dir_path, casper_folder, "bin", "casperjs"), os.path.join(dir_path, "screenshot.js"), dashboard_url, "15", "test"])
+
+    return True
 
 
 if __name__ == "__main__":
@@ -15,12 +54,15 @@ if __name__ == "__main__":
         print >> sys.stderr, "FATAL Unsupported execution mode (expected --execute flag)"
         sys.exit(1)
     try:
-        settings = json.loads(sys.stdin.read()) 
-        session_key = settings.get('session_key')
-        dashboard = settings['configuration'].get('dashboard')
-        app = settings['configuration'].get('app')
-        print >> sys.stderr, "Key: %s, Dashboard: %s, App: %s" % (session_key, dashboard, app)    
-        if not send_dashboard(session_key, dashboard, app):
+        payload = json.loads(sys.stdin.read())
+        print >> sys.stderr, "DEBUG payload from Splunk in JSON format: %s" % payload
+        server_uri = get_server_uri(payload.get('results_link'))
+        session_key = payload.get('session_key')
+        dashboard = payload['configuration'].get('dashboard')
+        app = payload['configuration'].get('app')
+        print >> sys.stderr, "DEBUG Dashboard name: %s, App name: %s, Session_Key: %s" % (dashboard, app,session_key)
+        dashboard_url = build_dashboard_url(server_uri, session_key, dashboard, app)
+        if not create_screenshot_dashboard(dashboard_url):
             sys.exit(2)
     except Exception, e:
         print >> sys.stderr, "ERROR Unexpected error: %s" % e
